b37aabdab23576827fac5b05ca60d7b3027f1781
Kegrathil <willkegrathil@gmail.com>
2025-02-23 01:16:42 +0100

    steam multiplayer

--- levels/network_manager.gd
+++ levels/network_manager.gd
@@ -3,13 +3,24 @@ extends Node
 @export var ui : UIManager
 
 const PLAYER = preload("res://player/player.tscn")
-const PORT = 6969
-var enet_peer = ENetMultiplayerPeer.new()
+
+var lobby_id = 0
+var steam_peer = SteamMultiplayerPeer.new()
+
+func _ready() -> void:
+	OS.set_environment("SteamAppID", str(480))
+	OS.set_environment("SteamGameID", str(480))
+	Steam.steamInitEx()
+	
+	steam_peer.lobby_created.connect(_on_lobby_created)
+
+func _process(_delta: float) -> void:
+	Steam.run_callbacks()
 
 
 func _on_host_pressed() -> void:
-	enet_peer.create_server(PORT)
-	multiplayer.multiplayer_peer = enet_peer
+	steam_peer.create_lobby(SteamMultiplayerPeer.LOBBY_TYPE_PUBLIC)
+	multiplayer.multiplayer_peer = steam_peer
 	
 	multiplayer.peer_connected.connect(add_player)
 	multiplayer.peer_disconnected.connect(remove_player)
@@ -22,34 +33,29 @@ func _on_host_pressed() -> void:
 
 
 func _on_join_pressed() -> void:
-	enet_peer.create_client("localhost", PORT)
-	multiplayer.multiplayer_peer = enet_peer
+	steam_peer.connect_lobby(ui.get_lobby_id())
+	multiplayer.multiplayer_peer = steam_peer
 	
 	$Camera3D.queue_free()
 	ui.toggle_network_menu(false)
 	
-	
+
+func _on_lobby_created(connected, id):
+	if connected:
+		lobby_id = id
+		Steam.setLobbyData(lobby_id, "name", str(Steam.getPersonaName()+"'s lobby"))
+		Steam.setLobbyJoinable(id, true)
+		print("Lobby created! ID: %s" % id)
+
 func add_player(peer_id):
 	var player = PLAYER.instantiate()
 	player.name = str(peer_id)
 	print("playername: " + player.name)
 	get_owner().add_child(player)
 
+
 func remove_player(peer_id):
 	var players = get_tree().get_nodes_in_group("player")
 	for player in players:
 		if player.name == str(peer_id):
 			player.queue_free()
-
-func upnp_setup():
-	var upnp = UPNP.new()
-	
-	var discover_result = upnp.discover()
-	assert(discover_result == UPNP.UPNP_RESULT_SUCCESS, "UPNP discover failed! Error: %s" % discover_result)
-	
-	assert(upnp.get_gateway() and upnp.get_gateway().is_valid_gateway(), "UPNP invalid gateway! Gateway: %s" % upnp.get_gateway())
-	
-	var map_result = upnp.add_port_mapping(PORT)
-	assert(map_result == UPNP.UPNP_RESULT_SUCCESS, "UPNP port mapping failed! Error: %s" % map_result)
-	
-	print("Success! Join address: %s" % upnp.query_external_address())
--- ui/ui_manager.gd
+++ ui/ui_manager.gd
@@ -2,6 +2,10 @@ extends Control
 
 class_name UIManager
 
+@onready var lobby_id_text_field: TextEdit = $VBoxContainer/LobbyIDTextField
 
 func toggle_network_menu(value : bool):
 	$VBoxContainer.visible = value
+
+func get_lobby_id() -> int:
+	return int(lobby_id_text_field.text)
