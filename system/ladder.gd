291d2427effb40379f8d2d61536e1ec0d14b38ec
Kegrathil <willkegrathil@gmail.com>
2025-05-15 10:03:35 +0200

    ladder stacking, and model/textures

--- system/ladder.gd
+++ system/ladder.gd
@@ -1,6 +1,16 @@
 extends Node3D
+class_name Ladder
 
 @onready var exit_point: Node3D = $ExitPoint
+@onready var top_bits: MeshInstance3D = $Cylinder_010
+
+
+func _ready() -> void:
+	await get_tree().create_timer(0.1).timeout
+	var parent_ladder := get_parent() as Ladder
+	if parent_ladder:
+		top_bits.visible = false
+		exit_point = parent_ladder.exit_point
 
 
 func _on_static_body_3d_interacted(source) -> void:
