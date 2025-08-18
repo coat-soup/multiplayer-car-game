aaed4b1390f7511212af9adde9d90e84d8bb8007
Kegrathil <willkegrathil@gmail.com>
2025-08-18 19:19:50 +0200

    compute shader camera data

--- vfx/scripts/cloud_compute.gd
+++ vfx/scripts/cloud_compute.gd
@@ -3,18 +3,26 @@ extends Node
 @export var texture_rect: TextureRect
 @export var color : Color
 
+@onready var cam : Camera3D = get_viewport().get_camera_3d()
+var rd : RenderingDevice
+var pipeline : RID
+var uniform_set : RID
+var color_buffer : RID
+var cam_buffer : RID
+
+var timer : float = 0.0
 
 func _ready():
-	var rd := RenderingServer.get_rendering_device()
+	rd = RenderingServer.get_rendering_device()
 	
 	var shader_file = preload("res://vfx/shaders/sphere_compute.glsl")
 	var shader := rd.shader_create_from_spirv(shader_file.get_spirv())
-	var pipeline := rd.compute_pipeline_create(shader)
+	pipeline = rd.compute_pipeline_create(shader)
 	
 	var input_data := PackedFloat32Array([color.r, color.g, color.b]).to_byte_array()
-	var storage_buffer := rd.storage_buffer_create(input_data.size(), input_data)
-	
-	
+	color_buffer = rd.storage_buffer_create(input_data.size(), input_data)
+	var cam_data := get_cam_data(cam)
+	cam_buffer = rd.storage_buffer_create(cam_data.size(), cam_data)
 	
 	var image := preload("res://vfx/textures/test_compute.png").get_image()
 	image.convert(Image.FORMAT_RGBAF)
@@ -34,44 +42,53 @@ func _ready():
 	var texture := rd.texture_create(texture_format, texture_view, [image.get_data()])
 	
 	
-	
-	
-	# RUNNING THE SHADER:
-	
 	var parameter_uniform := RDUniform.new()
 	parameter_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
 	parameter_uniform.binding = 0
-	parameter_uniform.add_id(storage_buffer)
+	parameter_uniform.add_id(color_buffer)
 	
 	var image_uniform := RDUniform.new()
 	image_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
 	image_uniform.binding = 1
 	image_uniform.add_id(texture)
 	
+	var cam_uniform := RDUniform.new()
+	cam_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	cam_uniform.binding = 2
+	cam_uniform.add_id(cam_buffer)
 	
-	var uniform_set := rd.uniform_set_create([parameter_uniform, image_uniform], shader, 0)
+	uniform_set = rd.uniform_set_create([parameter_uniform, image_uniform, cam_uniform], shader, 0)
+
+	
+	
+	# get modified image and display
+	var texture_rd := Texture2DRD.new()
+	texture_rd.texture_rd_rid = texture
+	texture_rect.texture = texture_rd
+
+
+func _process(delta: float) -> void:
+	if not cam:
+		cam = get_viewport().get_camera_3d()
+	
+	var cam_data = get_cam_data(cam)
+	rd.buffer_update(cam_buffer, 0, cam_data.size(), cam_data)
+	
+	timer += delta / 10.0
+	color = lerp(Color.CORNFLOWER_BLUE, Color.ORANGE, (sin(timer)))
+	var input_data := PackedFloat32Array([color.r, color.g, color.b]).to_byte_array()
+	rd.buffer_update(color_buffer, 0, input_data.size(), input_data)
+	
 	var compute_list := rd.compute_list_begin()
 	
 	rd.compute_list_bind_compute_pipeline(compute_list, pipeline)
 	rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0)
 	rd.compute_list_dispatch(compute_list, 32, 32, 1)
 	rd.compute_list_end()
-	
-	 
-	
-	run_compute()
-	
-	# get modified image and display
-	var texture_rd := Texture2DRD.new()
-	texture_rd.texture_rd_rid = texture
-	texture_rect.texture = texture_rd
-	
-	rd.free_rid(storage_buffer)
 
 
-func run_compute() -> void:
-	for x in range(32 * 32):
-		for y in range(32 * 32):
-			for z in range(1 * 1):
-				# run shader
-				var gl_GlobalInvocationID := Vector3(x,y,z)
+func get_cam_data(camera: Camera3D) -> PackedByteArray:
+	return PackedFloat32Array(
+		[camera.global_position.x, camera.global_position.y, camera.global_position.z,
+		camera.global_basis.z.x, camera.global_basis.z.y, camera.global_basis.z.z]
+	).to_byte_array()
--- vfx/shaders/sphere_compute.glsl
+++ vfx/shaders/sphere_compute.glsl
@@ -1,21 +1,44 @@
 #[compute]
 #version 450
 
-layout(set = 0, binding = 0, std430) readonly buffer parameters {
+layout(set = 0, binding = 0, std430) readonly buffer colour_multiplier {
     float r;
     float g;
     float b;
-} params;
+} col;
 
 layout(set = 0, binding = 1, rgba32f) uniform image2D image;
+
+layout(set = 0, binding = 2, std430) readonly buffer camera_transform {
+    vec3 pos;
+    vec3 forward;
+} cam;
 
 layout(local_size_x = 32, local_size_y = 32, local_size_z = 1) in;
 
 void main() {
     ivec2 uv = ivec2(gl_GlobalInvocationID.xy);
-    vec4 color = imageLoad(image, uv);
-    vec4 multiplier = vec4(params.r, params.g, params.b, 1.0);
 
-    vec4 output_color = color * multiplier;
+    // Simple depth approximation: distance along camera forward
+    vec3 frag_pos = vec3(uv, 0.0); // placeholder, normally you'd have world/view position
+    float depth = dot(frag_pos - cam.pos, cam.forward);
+
+    // Fog factor: linear fog with start/end distances
+    float fog_start = 10.0;
+    float fog_end = 50.0;
+    float fog_factor = clamp((depth - fog_start) / (fog_end - fog_start), 0.0, 1.0);
+
+    // Fog color
+    vec3 fog_color = vec3(0.5); // gray fog
+
+    // Original color
+    vec4 output_color = vec4(fog_color.x, fog_color.y, fog_color.z, fog_factor);
+
+    // Mix with fog
+    output_color.rgb = mix(output_color.rgb, fog_color, fog_factor);
+
+    //DEBUG
+    output_color = vec4(col.r, col.g, col.b, 1.0) * vec4(sin(cam.pos.x));
+
     imageStore(image, uv, output_color);
 }
