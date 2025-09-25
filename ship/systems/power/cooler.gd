7e87b170a6275fa3cbd64e60f0e261a4120ec458
Kegrathil <willkegrathil@gmail.com>
2025-09-25 21:03:38 +0200

    hammerhead textures

--- ship/systems/power/cooler.gd
+++ ship/systems/power/cooler.gd
@@ -3,31 +3,34 @@ class_name Cooler
 
 @export var cooling_rate : float = 5.0
 @export var particles : Array[GPUParticles3D]
+@onready var audio: AudioStreamPlayer3D = $AudioStreamPlayer3D
+
 
 func _ready() -> void:
 	super._ready()
-	toggle_particles(ship != null)
+	toggle_effects(ship != null)
 
 func on_install_to_ship(ship_manager : ShipManager):
 	super.on_install_to_ship(ship_manager)
 	ship_manager.cooling_manager.add_cooler_component(self)
-	toggle_particles(true)
+	toggle_effects(true)
 
 
 func on_uninstall_from_ship(ship_manager : ShipManager):
 	super.on_uninstall_from_ship(ship_manager)
 	ship_manager.cooling_manager.remove_cooler_component(self)
-	toggle_particles(false)
+	toggle_effects(false)
 
 
-func toggle_particles(value : bool):
+func toggle_effects(value : bool):
 	for particle in particles:
 		particle.emitting = value
+	audio.playing = value
 
 func on_break():
 	super.on_break()
-	toggle_particles(false)
+	toggle_effects(false)
 
 func on_fixed(source_id):
 	super.on_fixed(source_id)
-	if ship: toggle_particles(true)
+	if ship: toggle_effects(true)
